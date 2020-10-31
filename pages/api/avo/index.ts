2926c1fd4fcc935601786c1a18ff3d0a157665e0
Gerardo Villa <gera.villa21@gmail.com>
2020-10-31 11:30:29 -0600

    add api for allAvos

---
+++ pages/api/avo/index.ts
@@ -0,0 +1,19 @@
+import { IncomingMessage, ServerResponse } from 'http'
+import DB from '@database'
+
+const allAvos = async (request: IncomingMessage, response: ServerResponse) => {
+  const db = new DB()
+  const allEntries = await db.getAll()
+  const count = allEntries.length
+
+  response.statusCode = 200
+  response.setHeader('Content-type', 'application/json')
+  response.end(
+    JSON.stringify({
+      count,
+      data: allEntries,
+    })
+  )
+}
+
+export default allAvos
