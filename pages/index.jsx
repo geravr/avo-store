cd921594a231294e0135d68ce57ed814d231fdee
Gerardo Villa <gera.villa21@gmail.com>
2020-11-02 21:38:41 -0600

    show spinner when data is loading

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,17 +2,24 @@ import React, { useEffect, useState } from 'react'
 
 // Components
 import ProductList from '@components/ProductList/ProductList'
+import Spinner from '@components/Spinner/Spinner'
 
 const HomePage = () => {
   const [avocadoList, setAvocadoList] = useState([])
+  const [isLoading, setIsloading] = useState(true)
 
   useEffect(() => {
     fetch('/api/avo')
       .then((response) => response.json())
       .then(({ count, data }) => {
         setAvocadoList(data)
+        setIsloading(false)
       })
   }, [])
+
+  if (isLoading) {
+    return <Spinner size="12vh" />
+  }
 
   return (
     <div>
